eb646dd74014c6542f9f627158e26d249e63d4e6
Rutik Vasani <rutikvasani370@gmail.com>
2024-02-27 16:02:11 +0530

    new commit

---
+++ src/component/card/card.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import './card.css';
+
+const Card = () => {
+
+    return (
+        <>
+            <div className="body">
+                <div className="box">
+                    <h4>Name: vishu Bhingaradiya<br />
+                        Mobile: 9876543213<br />
+                        reason: Weight loss<br />
+                        End Date: 12/3/2023<br />
+                        <button className="btn">Accept</button>
+                        <button className="btn-2">Decline</button>
+                    </h4>
+                </div>
+            </div>
+            
+            <div className="body">
+                <div className="box">
+                    <h4>Name: vishu Bhingaradiya<br />
+                        Mobile: 9876543213<br />
+                        reason: Weight loss<br />
+                        End Date: 12/3/2023<br />
+                        <button className="btn">Accept</button>
+                        <button className="btn-2">Decline</button>
+                    </h4>
+                </div>
+            </div>
+            <div className="body">
+                <div className="box">
+                    <h4>Name: vishu Bhingaradiya<br />
+                        Mobile: 9876543213<br />
+                        reason: Weight loss<br />
+                        End Date: 12/3/2023<br />
+                        <button className="btn">Accept</button>
+                        <button className="btn-2">Decline</button>
+                    </h4>
+                </div>
+            </div>
+            <div className="body">
+                <div className="box">
+                    <h4>Name: vishu Bhingaradiya<br />
+                        Mobile: 9876543213<br />
+                        reason: Weight loss<br />
+                        End Date: 12/3/2023<br />
+                        <button className="btn">Accept</button>
+                        <button className="btn-2">Decline</button>
+                    </h4>
+
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default Card;
